0e68e0feecaf40879d1f63d929069657ffbf4f0b
ouafa ben amor <ouafa.benamor@gmail.com>
2022-11-10 13:58:43 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+// pb1
+// console.log("hello world")
+// pb2
+// const http = require('http');
+
+// const requestListener = function (req, res) {
+//   res.writeHead(200);
+//   res.end('<h1>hello node<h1/>');
+// }
+
+// const server = http.createServer(requestListener);
+// server.listen(3000);
+// pb3
+// const fs=require("fs")
+// fs.readFile('./welcome.txt',"utf8",(err,data)=>{
+//     err ? console.log(err) : console.log(data)
+// })
